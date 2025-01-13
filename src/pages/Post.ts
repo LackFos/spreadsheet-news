84ebfb30ef124acebb64bdeee9d894345a6175d1
LackFos <LackFosisOnline@gmail.com>
2025-01-13 23:28:07 +0700

    Add Error500 page

--- src/pages/Post.ts
+++ src/pages/Post.ts
@@ -2,14 +2,23 @@
 import { RouterCallback } from '../types';
 import query from '../utils/query';
 import Error404 from './error/Error404';
+import Error500 from './error/Error500';
 
 const Post = async ({ env, params, setStatusCode }: RouterCallback) => {
 	const { SHEETID } = env as Record<string, string>;
 	const slug = params?.slug;
+	let data = null;
 
-	const data = (
-		await query(`https://docs.google.com/spreadsheets/d/${SHEETID}/gviz/tq?tqx=out:json&headers=1&tq=SELECT * WHERE B = '${slug}' limit 1`)
-	)[0];
+	try {
+		data = (
+			await query(
+				`https://docs.google.com/spreadsheets/d/${SHEETID}/gviz/tq?tqx=out:json&headers=1&tq=SELECT * WHERE B = '${slug}' limit 1`
+			)
+		)[0];
+	} catch (error) {
+		setStatusCode(500);
+		return Error500();
+	}
 
 	if (!data) {
 		setStatusCode(404);
---
+++ src/pages/error/Error500.ts
@@ -0,0 +1,14 @@
+const Error500 = () => {
+	return `
+        <div class="container__center error">
+            <h1>Error 500</h1>
+            <p>- Internal Server Error -</p>
+            <div class="back">
+                Back to 
+                <a class="link" href="/">Home</a>
+            </div>
+        </div>
+    `;
+};
+
+export default Error500;
