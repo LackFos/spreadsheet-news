7957427e1c4b1f823b827391543c6603a4373ad2
LackFos <LackFosisOnline@gmail.com>
2025-01-26 15:11:05 +0700

    Fix invalid rss format

--- src/libs/RssGenerator.ts
+++ src/libs/RssGenerator.ts
@@ -9,33 +9,41 @@ interface Item {
 }
 
 class RssGenerator {
+	private domain: string;
 	private items: Item[] = [];
+
+	constructor(domain: string) {
+		this.domain = domain;
+	}
 
 	addItem(title: string, link: string, description: string, pubDate: Date): void {
 		const date = new Date(pubDate);
-		const formattedDate = format(date, 'EEEE, dd MMMM yyyy');
-		this.items.push({ title, link, description, pubDate: formattedDate });
+		const formattedDate = format(date, 'EEE, dd MMM yyyy HH:mm:ss xx');
+		this.items.push({ title, link: this.domain + link, description, pubDate: formattedDate });
 	}
 
 	generate(): string {
 		const rss = `
 		<?xml version="1.0" encoding="UTF-8"?>
-		<rss version="2.0">
-		    <title>Bloqu</title>
-		    <link>https://www.bloqu.com</link>
-			<description>Stay informed with Bloqu. Discover unbiased reporting, insightful stories, and fresh perspectives from trusted independent voices.</description>
-
-			${this.items
-				.map(
-					(item) =>
-						`<item>
-							<title>${item.title}</title>
-							<link>${item.link}</link>
-							<description>${item.description}</description>
-							<pubDate>${item.pubDate}</pubDate>
-						</item>`
-				)
-				.join('')}
+		<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+		   <channel>
+				<title>Bloqu</title>
+				<link>${this.domain}</link>
+				<atom:link href="${this.domain}/rss.xml" rel="self" type="application/rss+xml"/>
+				<description>Stay informed with Bloqu. Discover unbiased reporting, insightful stories, and fresh perspectives from trusted independent voices.</description>
+				${this.items
+					.map(
+						(item) =>
+							`<item>
+								<title>${item.title}</title>
+								<link>${item.link}</link>
+								<description>${item.description}</description>
+								<pubDate>${item.pubDate}</pubDate>
+								<guid>${item.link}</guid>
+							</item>`
+					)
+					.join('')}  
+		   </channel>
 		</rss>`;
 
 		return minify(rss);
--- src/pages/Rss.ts
+++ src/pages/Rss.ts
@@ -7,8 +7,8 @@ const Rss = async ({ env, url, params }: RouterCallback) => {
 
 	const posts = await query(`https://docs.google.com/spreadsheets/d/${SHEETID}/gviz/tq?tqx=out:json&headers=1&tq=SELECT * LIMIT 50`);
 
-	const RssGenarator = new RssGenerator();
-	posts.forEach((post) => RssGenarator.addItem(post.name, `${APP_URL}/post/${post.slug}`, post.description, new Date(post.created_at)));
+	const RssGenarator = new RssGenerator(APP_URL);
+	posts.forEach((post) => RssGenarator.addItem(post.name, `/post/${post.slug}`, post.description, new Date(post.created_at)));
 	return RssGenarator.generate();
 };
 
