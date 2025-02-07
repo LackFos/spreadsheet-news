4912fe7e244d333c5d0f7e419e4302762911aaa6
LackFos <LackFosisOnline@gmail.com>
2025-02-07 23:09:57 +0700

    Fix rss page exception

--- src/pages/Rss.ts
+++ src/pages/Rss.ts
@@ -7,10 +7,13 @@ const Rss = async ({ env, url, params }: RouterCallback) => {
 	const SHEETID = (env as Record<string, string>).SHEETID;
 	const APP_URL = await useConfig(SHEETID, 'app_url');
 
-	const posts = await query(`https://docs.google.com/spreadsheets/d/${SHEETID}/gviz/tq?tqx=out:json&headers=1&tq=SELECT * LIMIT 50`);
+	const posts = await query(`https://docs.google.com/spreadsheets/d/${SHEETID}/gviz/tq?tqx=out:json&headers=1&tq=SELECT * LIMIT 10`);
 
 	const RssGenarator = new RssGenerator(APP_URL);
+
 	posts.forEach((post) => RssGenarator.addItem(post.name, `/post/${post.slug}`, post.description, new Date(post.created_at)));
+	console.log(RssGenarator.generate());
+
 	return RssGenarator.generate();
 };
 
